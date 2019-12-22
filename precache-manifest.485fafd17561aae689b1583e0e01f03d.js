041aaf8f7db89ad879cb921b8a22f1694c59e935
Boris Brtáň <39240194+boris-brtan@users.noreply.github.com>
2019-12-22 17:18:08 +0100

    Update

---
+++ precache-manifest.485fafd17561aae689b1583e0e01f03d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a479fa4977036af5bd9a",
+    "url": "/css/app.0d7b7e0c.css"
+  },
+  {
+    "revision": "b80c4d98686bfb5674c4",
+    "url": "/css/chunk-vendors.7caec713.css"
+  },
+  {
+    "revision": "9222c5d154c24a4b08d8",
+    "url": "/css/donation.ad3db9ae.css"
+  },
+  {
+    "revision": "b5bac89edd8eb74043a7",
+    "url": "/css/user.f7198c68.css"
+  },
+  {
+    "revision": "b5016e3a3117dec691edcd6751beb813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a479fa4977036af5bd9a",
+    "url": "/js/app.4e986a08.js"
+  },
+  {
+    "revision": "b80c4d98686bfb5674c4",
+    "url": "/js/chunk-vendors.84030f69.js"
+  },
+  {
+    "revision": "9222c5d154c24a4b08d8",
+    "url": "/js/donation.0f2b32b5.js"
+  },
+  {
+    "revision": "b5bac89edd8eb74043a7",
+    "url": "/js/user.947dd86c.js"
+  },
+  {
+    "revision": "2e84543799fa3e907a68a345cce8900f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
